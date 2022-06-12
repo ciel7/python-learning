e859ca2af0b53dbafadbf6441b01469e621deaa0
huxiaoting01 <huxiaoting01@baidu.com>
2022-06-12 18:54:20 +0800

    电影信息的抓取、存储

--- data_analysis/C2/4_连续获取多个页面信息.py
+++ data_analysis/C2/4_连续获取多个页面信息.py
@@ -4,6 +4,12 @@
 @Name: 4_连续获取多个页面信息.py
 @Auth: ciel7
 @Date: 2022/6/12-下午6:50
-@Desc: 
+@Desc: 访问豆瓣 top250 主页，跳转下一页，实现每个页面信息的抓取
 @Ver : 
 """
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
